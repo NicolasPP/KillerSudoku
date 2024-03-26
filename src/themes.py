f8f7f28a856dfacbbd82ed70ed6ab8475106873c
Nicolas <nicolas.parucker@hotmail.com>
2024-03-26 19:43:06 +0700

    Changed pygame import

--- src/themes.py
+++ src/themes.py
@@ -1,47 +1,47 @@
 from typing import NamedTuple
 
-import pygame
+from pygame.color import Color
 
 from puzzle_store import PuzzleDifficulty
 
 
 class GameTheme(NamedTuple):
-    foreground_primary: pygame.color.Color
-    foreground_secondary: pygame.color.Color
-    background_primary: pygame.color.Color
-    background_secondary: pygame.color.Color
+    foreground_primary: Color
+    foreground_secondary: Color
+    background_primary: Color
+    background_secondary: Color
 
 
 class DifficultyThemes:
     themes: dict[PuzzleDifficulty, GameTheme] = {
         PuzzleDifficulty.EASY: GameTheme(
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(255, 255, 255),
-            pygame.color.Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
         ),
         PuzzleDifficulty.NORMAL: GameTheme(
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(255, 255, 255),
-            pygame.color.Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
         ),
         PuzzleDifficulty.HARD: GameTheme(
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(255, 255, 255),
-            pygame.color.Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
         ),
         PuzzleDifficulty.EXPERT: GameTheme(
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(255, 255, 255),
-            pygame.color.Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
         ),
         PuzzleDifficulty.MASTER: GameTheme(
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(0, 0, 0),
-            pygame.color.Color(255, 255, 255),
-            pygame.color.Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
         )
     }
