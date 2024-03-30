b4b6e96f4fce315269d66ac7ff835fa7ff97cf01
Nicolas <nicolas.parucker@hotmail.com>
2024-03-30 08:56:28 +0700

    added default themes

--- src/themes.py
+++ src/themes.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import NamedTuple
 
 from pygame.color import Color
@@ -6,6 +8,15 @@ from puzzle_store import PuzzleDifficulty
 
 
 class GameTheme(NamedTuple):
+    @staticmethod
+    def default() -> GameTheme:
+        return GameTheme(
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 255),
+        )
+
     foreground_primary: Color
     foreground_secondary: Color
     background_primary: Color
@@ -13,6 +24,7 @@ class GameTheme(NamedTuple):
 
 
 class DifficultyThemes:
+
     themes: dict[PuzzleDifficulty, GameTheme] = {
         PuzzleDifficulty.EASY: GameTheme(
             Color(0, 0, 0),
