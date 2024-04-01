a5d0763e430ddcf203927abd9abbb1e3cd50b7dc
Nicolas <nicolas.parucker@hotmail.com>
2024-04-01 20:24:07 +0700

    added some constants

--- src/app_config.py
+++ src/app_config.py
@@ -1,13 +1,19 @@
 APP_WIDTH: int = 600
 GAME_HEIGHT: int = 750
 MAX_PUZZLES: int = 100
-PUZZLE_DATA_PATH: str = "data"
 FONT_SIZE: int = 40
 CAGE_PAD: int = 5
-TITLE: str = "Killer Sudoku"
 HOVER_ALPHA: int = 100
 BOARD_SIZE: int = 9
+TITLE: str = "Killer Sudoku"
+PUZZLE_DATA_PATH: str = "data"
 
+# Assets
+ICONS: str = r"assets\icons"
+BACK_ICON: str = "arrow-small-left"
+ERASER_ICON: str = "eraser"
+PENCIL_ICON: str = "pencil"
+DEFAULT_ICON_SIZE: int = 50
 
 # Page Ids
 MAIN_MENU_PAGE: int = 1
