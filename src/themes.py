c2112451780e0dc2fbfaff7cf70039c07ef91801
Nicolas <nicolas.parucker@hotmail.com>
2024-04-03 18:22:02 +0700

    easy theme is now dark to check if the theme is updating correctly

--- src/themes.py
+++ src/themes.py
@@ -27,10 +27,10 @@ class DifficultyThemes:
 
     themes: dict[PuzzleDifficulty, GameTheme] = {
         PuzzleDifficulty.EASY: GameTheme(
-            Color(0, 0, 0),
-            Color(0, 0, 0),
             Color(255, 255, 255),
             Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
         ),
         PuzzleDifficulty.NORMAL: GameTheme(
             Color(0, 0, 0),
