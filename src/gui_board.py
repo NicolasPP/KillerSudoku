099cd6698e450e7a9e76ce64c2f92ed3214f7f97
Nicolas <nicolas.parucker@hotmail.com>
2024-04-04 21:39:07 +0700

    drawing corners of cages

--- src/gui_board.py
+++ src/gui_board.py
@@ -1,8 +1,8 @@
 from enum import Enum
 from enum import auto
+from functools import cache
 from itertools import chain
 from queue import Queue
-from typing import NamedTuple
 from typing import Optional
 from typing import override
 
@@ -18,7 +18,8 @@ from pygame.rect import Rect
 from pygame.surface import Surface
 
 from config.app_config import BOARD_SIZE
-from config.app_config import CAGE_PAD
+from config.game_config import CAGE_PAD
+from config.game_config import CELL_PAD
 from config.game_config import SUM_FONT_SIZE
 from events import AppEvent
 from gui_component import GuiComponent
@@ -33,11 +34,22 @@ class Direction(Enum):
     LEFT = auto()
     RIGHT = auto()
 
+    def get(self) -> tuple[int, int]:
+        direction: Direction = Direction[self.name]
+        if direction is Direction.UP:
+            return -1, 0
 
-class Neighbour(NamedTuple):
-    direction: Direction
-    row: int
-    col: int
+        elif direction is Direction.DOWN:
+            return 1, 0
+
+        elif direction is Direction.LEFT:
+            return 0, -1
+
+        elif direction is Direction.RIGHT:
+            return 0, 1
+
+        else:
+            raise Exception()
 
 
 class Cell:
@@ -57,6 +69,7 @@ class BoardGui(GuiComponent):
         if self._require_redraw:
             self._draw_board_vals()
             self._draw_cages()
+            self._require_redraw = False
 
         self._render_selected()
         self.parent.surface.blit(self._surface,
@@ -117,53 +130,62 @@ class BoardGui(GuiComponent):
 
     def _create_board_surface(self) -> Surface:
         cells: list[list[Cell]] = []
-        pad: int = 1
-        cell_size: int = (min(self.parent.surface.get_width(), self.parent.surface.get_height()) // BOARD_SIZE) - (pad * 2)
-        board: Surface = Surface(Vector2((cell_size * BOARD_SIZE) + (14 * pad)))
-        row_padding: int = pad
+        cell_size: int = (min(self.parent.surface.get_width(), self.parent.surface.get_height()) // BOARD_SIZE) - \
+                         (CELL_PAD * 2)
+        board: Surface = Surface(Vector2((cell_size * BOARD_SIZE) + (14 * CELL_PAD)))
+        row_padding: int = CELL_PAD
         for row in range(BOARD_SIZE):
             region_row: list[Cell] = []
-            col_padding: int = pad
+            col_padding: int = CELL_PAD
             for col in range(BOARD_SIZE):
-                placement: Rect = Rect((col * (cell_size + pad)) + col_padding, (row * (cell_size + pad)) + row_padding,
-                                       cell_size, cell_size)
+                placement: Rect = Rect((col * (cell_size + CELL_PAD)) + col_padding,
+                                       (row * (cell_size + CELL_PAD)) + row_padding, cell_size, cell_size)
                 surface: Surface = Surface(placement.size)
 
                 if (col + 1) % 3 == 0:
-                    col_padding += pad * 2
+                    col_padding += CELL_PAD * 2
 
                 region_row.append(Cell(Region(board, surface, placement), row, col))
 
             if (row + 1) % 3 == 0:
-                row_padding += pad * 2
+                row_padding += CELL_PAD * 2
 
             cells.append(region_row)
 
         self._cells = cells
         return board
 
-    def _get_neighbours(self, row: int, col: int) -> list[Neighbour]:
-        neighbours: list[Neighbour] = []
+    @cache
+    def _get_neighbours(self, row: int, col: int) -> set[Direction]:
+        neighbours: set[Direction] = set()
         if row >= len(self._cells) or row < 0:
             return neighbours
 
         if col >= len(self._cells[0]) or col < 0:
             return neighbours
 
-        for row_offset, col_offset, direction in \
-                [(1, 0, Direction.DOWN), (-1, 0, Direction.UP), (0, 1, Direction.RIGHT), (0, -1, Direction.LEFT)]:
-            n_row: int = row + row_offset
-            n_col: int = col + col_offset
+        for direction in [Direction.DOWN, Direction.UP, Direction.RIGHT, Direction.LEFT]:
+            index: Vector2 = Vector2(row, col) + Vector2(direction.get())
 
-            if n_row >= len(self._cells) or n_row < 0:
+            if index.x >= len(self._cells) or index.x < 0:
                 continue
 
-            if n_col >= len(self._cells[0]) or n_col < 0:
+            if index.y >= len(self._cells[0]) or index.y < 0:
                 continue
 
-            neighbours.append(Neighbour(direction, n_row, n_col))
+            neighbours.add(direction)
 
         return neighbours
+
+    def _get_present_neighbours(self, row: int, col: int, present_cells: set[tuple[int, int]]) -> set[Direction]:
+        neighbours: set[Direction] = self._get_neighbours(row, col)
+        present: set[Direction] = set()
+        for neighbour in neighbours:
+            index: Vector2 = Vector2(row, col) + Vector2(neighbour.get())
+            if (index.x, index.y) in present_cells:
+                present.add(neighbour)
+
+        return present
 
     def _draw_cages(self) -> None:
         font: Font = SysFont(get_fonts()[0], SUM_FONT_SIZE)
@@ -174,37 +196,142 @@ class BoardGui(GuiComponent):
             sum_surface: Surface = font.render(str(cage_sum), True, self._theme.foreground_primary,
                                                self._theme.background_primary)
             for row, col in cells:
-                neighbours: list[Neighbour] = self._get_neighbours(row, col)
-                present_neighbours: list[Neighbour] = list(
-                    filter(lambda n: (n.row, n.col) in present_cells, neighbours))
+                neighbours: set[Direction] = self._get_present_neighbours(row, col, present_cells)
+                self._draw_cage_side(row, col, neighbours)
+                self._draw_cage_corner(row, col, neighbours, cells)
 
-                self._draw_cage_side(row, col, set([n.direction for n in present_neighbours]))
+            sum_cell.region.surface.blit(sum_surface, sum_surface.get_rect(center=(CAGE_PAD, CAGE_PAD)))
 
-            sum_cell.region.surface.blit(sum_surface, (0, 0))
+    def _draw_cage_corner(self, row: int, col: int, neighbours: set[Direction], cage: list[tuple[int, int]]) -> None:
+        cell_surface: Surface = self._cells[row][col].region.surface
+        cell_size: Vector2 = Vector2(cell_surface.get_size())
+        cell_w, cell_h = cell_surface.get_size()
+
+        def is_extra_cell_present(row_offset: int, col_offset: int) -> bool:
+            extra_row: int = row + row_offset
+            extra_col: int = col + col_offset
+            if extra_row >= len(self._cells) or extra_row < 0:
+                return False
+
+            if extra_col >= len(self._cells[0]) or extra_col < 0:
+                return False
+
+            return (extra_row, extra_col) in cage
+
+        is_left_present: bool = Direction.LEFT in neighbours
+        is_right_present: bool = Direction.RIGHT in neighbours
+        is_up_present: bool = Direction.UP in neighbours
+        is_down_present: bool = Direction.DOWN in neighbours
+
+        # bottom left corner
+        if is_down_present and is_left_present and not is_extra_cell_present(1, -1):
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(CAGE_PAD, cell_h), Vector2(CAGE_PAD, cell_h - CAGE_PAD))
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(CAGE_PAD, cell_h - CAGE_PAD), Vector2(0, cell_h - CAGE_PAD))
+
+        # bottom right corner
+        if is_down_present and is_right_present and not is_extra_cell_present(1, 1):
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      cell_size - Vector2(CAGE_PAD, 0), cell_size - Vector2(CAGE_PAD))
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      cell_size - Vector2(CAGE_PAD), cell_size - Vector2(0, CAGE_PAD))
+
+        # top left corner
+        if is_up_present and is_left_present and not is_extra_cell_present(-1, -1):
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(CAGE_PAD, 0), Vector2(CAGE_PAD, CAGE_PAD))
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(CAGE_PAD, CAGE_PAD), Vector2(0, CAGE_PAD))
+
+        # top right corner
+        if is_up_present and is_right_present and not is_extra_cell_present(-1, 1):
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(cell_w - CAGE_PAD, 0), Vector2(cell_w - CAGE_PAD, CAGE_PAD))
+            draw.line(cell_surface, self._theme.foreground_primary,
+                      Vector2(cell_w - CAGE_PAD, CAGE_PAD), Vector2(cell_w, CAGE_PAD))
 
     def _draw_cage_side(self, row: int, col: int, excluded_directions: set[Direction]) -> None:
         cell_surface: Surface = self._cells[row][col].region.surface
+        is_left_present: bool = Direction.LEFT in excluded_directions
+        is_right_present: bool = Direction.RIGHT in excluded_directions
+        is_up_present: bool = Direction.UP in excluded_directions
+        is_down_present: bool = Direction.DOWN in excluded_directions
+        start = end = Vector2(0)
+
         for direction in {Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT} - excluded_directions:
             if direction is Direction.UP:
-                draw.line(cell_surface, self._theme.foreground_primary,
-                          Vector2(CAGE_PAD), Vector2(cell_surface.get_width() - CAGE_PAD, CAGE_PAD))
+                if not is_left_present and not is_right_present:
+                    start = Vector2(CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, CAGE_PAD)
+
+                elif is_left_present and is_right_present:
+                    start = Vector2(0, CAGE_PAD)
+                    end = Vector2(cell_surface.get_width(), CAGE_PAD)
+
+                elif is_right_present:
+                    start = Vector2(CAGE_PAD)
+                    end = Vector2(cell_surface.get_width(), CAGE_PAD)
+
+                elif is_left_present:
+                    start = Vector2(0, CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, CAGE_PAD)
 
             elif direction is Direction.DOWN:
-                draw.line(cell_surface, self._theme.foreground_primary,
-                          Vector2(CAGE_PAD, cell_surface.get_height() - CAGE_PAD),
-                          Vector2(cell_surface.get_height() - CAGE_PAD, cell_surface.get_width() - CAGE_PAD))
+                if not is_left_present and not is_right_present:
+                    start = Vector2(CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
+
+                elif is_left_present and is_right_present:
+                    start = Vector2(0, cell_surface.get_height() - CAGE_PAD)
+                    end = Vector2(cell_surface.get_width(), cell_surface.get_height() - CAGE_PAD)
+
+                elif is_right_present:
+                    start = Vector2(CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
+                    end = Vector2(cell_surface.get_width(), cell_surface.get_height() - CAGE_PAD)
+
+                elif is_left_present:
+                    start = Vector2(0, cell_surface.get_height() - CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
 
             elif direction is Direction.LEFT:
-                draw.line(cell_surface, self._theme.foreground_primary,
-                          Vector2(CAGE_PAD), Vector2(CAGE_PAD, cell_surface.get_width() - CAGE_PAD))
+                if not is_down_present and not is_up_present:
+                    start = Vector2(CAGE_PAD)
+                    end = Vector2(CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
+
+                elif is_down_present and is_up_present:
+                    start = Vector2(CAGE_PAD, 0)
+                    end = Vector2(CAGE_PAD, cell_surface.get_height())
+
+                elif is_down_present:
+                    start = Vector2(CAGE_PAD)
+                    end = Vector2(CAGE_PAD, cell_surface.get_height())
+
+                elif is_up_present:
+                    start = Vector2(CAGE_PAD, 0)
+                    end = Vector2(CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
 
             elif direction is Direction.RIGHT:
-                draw.line(cell_surface, self._theme.foreground_primary,
-                          Vector2(cell_surface.get_height() - CAGE_PAD, CAGE_PAD),
-                          Vector2(cell_surface.get_height() - CAGE_PAD, cell_surface.get_width() - CAGE_PAD))
+                if not is_down_present and not is_up_present:
+                    start = Vector2(cell_surface.get_width() - CAGE_PAD, CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
+
+                elif is_down_present and is_up_present:
+                    start = Vector2(cell_surface.get_width() - CAGE_PAD, 0)
+                    end = Vector2(cell_surface.get_height() - CAGE_PAD, cell_surface.get_width())
+
+                elif is_down_present:
+                    start = Vector2(cell_surface.get_width() - CAGE_PAD, CAGE_PAD)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, cell_surface.get_height())
+
+                elif is_up_present:
+                    start = Vector2(cell_surface.get_width() - CAGE_PAD, 0)
+                    end = Vector2(cell_surface.get_width() - CAGE_PAD, cell_surface.get_height() - CAGE_PAD)
 
             else:
                 raise Exception(f"Direction: {direction.name} not recognised")
+
+            draw.line(cell_surface, self._theme.foreground_primary, start, end)
 
     def _get_collision_offset(self) -> Vector2:
         return Vector2(self.parent.placement.topleft) + \
